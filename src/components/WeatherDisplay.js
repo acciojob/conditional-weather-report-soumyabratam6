b821b3e2753a83994e647af8f7ec315e2b3a925a
soumyabrata <soumyabratam6@gmail.com>
2024-08-05 20:20:57 +0530

    weatherdisplay.js modify

--- src/components/WeatherDisplay.js
+++ src/components/WeatherDisplay.js
@@ -3,7 +3,7 @@ function WeatherDisplay({weather}){
     const tempColor = weather.temperature >20? "red":"blue";
     return(
         <>
-            <p>Temperature:<span style={{color:tempColor}}>{weather.temperature}</span></p>
+            <p>Temperature: <span style={{color:tempColor}}>{weather.temperature}</span></p>
             <p>Conditions: {weather.conditions}</p>
         </>
     );
